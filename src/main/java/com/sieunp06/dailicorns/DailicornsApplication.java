4802cd31b1fda6bcd9257aa4d25d09b0bb10ba3d
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-07-02 03:36:00 +0900

    Add: 초기 프로젝트 세팅
    `start.spring.io`를 사용하여 초기 프로젝트를 세팅하였고, 사용할 Depencies를 설정함.
    
    Dependecies
    - Spring Web
    - Lombok
    - PostgreSQL Driver

---
+++ src/main/java/com/sieunp06/dailicorns/DailicornsApplication.java
@@ -0,0 +1,13 @@
+package com.sieunp06.dailicorns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailicornsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailicornsApplication.class, args);
+	}
+
+}
